4b1c94d78ec3ae79f751d8d20188a4cee2eb4fc0
Seundede <omadoyedede0@gmail.com>
2024-11-11 21:49:40 +0100

    fix: quiz one solution

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,9 +11,18 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples (number_of_apples_brought: i32) -> i32 {
+    if number_of_apples_brought > 40 {
+        number_of_apples_brought
+    } else {
+        number_of_apples_brought * 2
+    }
 
+}
 fn main() {
     // You can optionally experiment here.
+    calculate_price_of_apples(40);
+    
 }
 
 // Don't change the tests!
